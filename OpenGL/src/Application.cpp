223a45db6333df18db3ec0fe29374528e17f1669
nstoilov99 <nstoilov99@gmail.com>
2025-05-09 18:24:41 +0300

    testing mvp matrices

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -32,7 +32,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -49,10 +49,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
         float positions[] = {
-			-0.5f, -0.5f, 0.0f, 0.0f,
-			 0.5f, -0.5f, 1.0f, 0.0f,
-			 0.5f,  0.5f, 1.0f, 1.0f,
-			-0.5f,  0.5f, 0.0f, 1.0f
+			 100.0f, 100.0f, 0.0f, 0.0f,
+			 200.0f, 100.0f, 1.0f, 0.0f,
+			 200.0f, 200.0f, 1.0f, 1.0f,
+			 100.0f, 200.0f, 0.0f, 1.0f
         };
 
         unsigned int indecies[] = {
@@ -73,12 +73,16 @@ int main(void)
 
         IndexBuffer ib(indecies, 6);
 
-		glm::mat4 projection = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+		glm::mat4 projection = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+		glm::mat4 mvp = projection * view * model;
 
         Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.9f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", projection);
+		shader.SetUniformMat4f("u_MVP", mvp);
 
         Texture texture("res/textures/StuckInGamin-logo.png");
 		texture.Bind();
